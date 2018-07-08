2feddf1ea99417414bad123a9f912fcc5c4a20d2
aun <aun.sira@gmail.com>
2018-07-08 12:55:11 +0700

    feat: Add utils

---
+++ lib/fake_kiyo/utils.rb
@@ -0,0 +1,14 @@
+require 'socket'
+
+module FakeKiyo
+  class Utils
+    FIND_AVAILABLE_PORT = 0
+
+    def self.find_available_port
+      server = TCPServer.new(FIND_AVAILABLE_PORT)
+      server.addr[1]
+    ensure
+      server.close if server
+    end
+  end
+end
